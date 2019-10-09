418c42701dd4bddd362ac7e49c91113640b70f0f
Orestis Malaspinas <orestis.malaspinas@hesge.ch>
2019-10-10 00:47:53 +0200

    added cmakes in examples as well as script to cmake + make

---
+++ examples/cmake_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+loc_dir=`pwd`
+echo ${loc_dir}
+
+for dir in `find . -name "build"  -type d`
+do
+    cd ${loc_dir}/${dir}
+    cmake ..
+done
+
+cd ${loc_dir}
+
+for dir in `find . -name "build"  -type d`
+do
+    cd ${loc_dir}/${dir}
+    make -j4
+done
