525a8de70ea9576d5dc16da1ee7b48f45f7896d6
Orestis <orestis.malaspinas@pm.me>
2022-02-01 00:55:12 +0000

    Regression test for unsigned types

--- examples/codesByTopic/unsignedTypes/unsignedTypes.cpp
+++ examples/codesByTopic/unsignedTypes/unsignedTypes.cpp
@@ -96,6 +96,9 @@ int main(int argc, char *argv[])
     std::unique_ptr<MultiScalarField2D<bool>> boolFlag1 =
         copyConvert<unsignedType, bool>(*evaluate(checkFlag1, flagField));
 
+    pcout << flagField << std::endl;
+    pcout << *boolFlag1 << std::endl;
+
     // Write images to show that only where the Flag1 was set, the evaluate returns true
     ImageWriter<unsignedType> imageWriter("leeloo.map");
     imageWriter.writeScaledGif("flagField", flagField, 1000, 1000);
